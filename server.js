03c7d7e7a37446b667212b67046dc0e1764ffbbf
aReallyBadDoctor <joshliefeld@gmail.com>
2022-04-13 01:24:56 -0400

    test
    aaaaa
    
    t
    
    t
    
    Update server.js
    
    fix
    
    fix

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
+<<<<<<< HEAD
+const http = require('http');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+
+http.createServer((req,res) => {
+	const fp = __dirname + (req.url === '/' ? '/index.html' : req.url);
+	fs.readFile(fp, (err, data) => {
+		if (err) {
+			res.writeHead(404);
+			res.end(JSON.stringify(err));
+			return;
+		}
+		res.writeHead(200);
+		res.end(data);
+	});
+}).listen(port);
+=======
 var http = require("http");
 
 http.createServer(function (req, res){
@@ -5,3 +24,4 @@ http.createServer(function (req, res){
 	res.end('Hello World\n');
 }).listen(8080);
 console.log(11000);
+>>>>>>> parent of 34e40ec (test)
