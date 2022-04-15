d4e2fdb33aaf6edc16b5be6477c1ede807612d60
aReallyBadDoctor <joshliefeld@gmail.com>
2022-04-15 00:12:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const http = require('http');
 const fs = require('fs');
 const WebSocketServer = require('ws').Server;
 
-
 const port = process.env.PORT || 3000;
 
 var hs = http.createServer((req,res) => {
@@ -18,17 +17,26 @@ var hs = http.createServer((req,res) => {
 	});
 });
 
-var wss = new WebSocketServer({hs});
-wss.on('connection', ((ws) => {
-	console.log("client connected");
+var wss = new WebSocketServer({server:hs});
+this.wss = wss;
+connections = {};
+wss.on('connection', ((ws,req) => {
+	var userID = parseInt(req.url.substr(1),10);
+	console.log(userID);
 	ws.on('message', (message) => {
-		console.log('message:' + message);
+		var data = JSON.parse(message);
+		if(data[0] == "response"){
+			return true;
+		}
+		else if(data[0] == "lighting"){
+			return true;
+		}
 	});
 	ws.on('end', ()=>{
 		console.log("connection ended");
 	});
 }));
-wss.on('listening', ()=>{console.log('WSS listening on port',config.get('port'))});
+wss.on('listening', ()=>{console.log('WSS listening on port',port)});
 wss.on('error', (e)=>{console.log("WSS error:", e);});
 hs.on('error', (e)=>{console.log("HS error:",e)});
 hs.listen(port);
